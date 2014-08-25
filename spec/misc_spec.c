29bc3fc6ce1957df9a99657fdb62951d1ba4350e
John Mettraux <jmettraux@gmail.com>
2014-08-25 21:28:04 +0900

    specify misc methods

---
+++ spec/misc_spec.c
@@ -0,0 +1,50 @@
+
+//
+// specifying gajeta
+//
+// Mon Aug 25 11:41:45 JST 2014
+//
+
+#include "gajeta.h"
+
+
+//char *fgaj_level_to_string(char level);
+//char fgaj_normalize_level(char level);
+context "misc"
+{
+  describe "fgaj_normalize_level()"
+  {
+    it "turns the input into a numeric level"
+    {
+      ensure(fgaj_normalize_level('t') == 10);
+      ensure(fgaj_normalize_level('d') == 20);
+      ensure(fgaj_normalize_level('i') == 30);
+      ensure(fgaj_normalize_level('e') == 40);
+      ensure(fgaj_normalize_level('w') == 50);
+    }
+
+    it "leaves numeric levels as is"
+    {
+      ensure(fgaj_normalize_level(10) == 10);
+    }
+  }
+
+  describe "fgaj_level_to_string()"
+  {
+    it "turns a char level into a string"
+    {
+      ensure(fgaj_level_to_string('t') === "TRACE");
+      ensure(fgaj_level_to_string('d') === "DEBUG");
+      ensure(fgaj_level_to_string('i') === "INFO");
+      ensure(fgaj_level_to_string('e') === "ERROR");
+      ensure(fgaj_level_to_string('w') === "WARN");
+
+      ensure(fgaj_level_to_string(10) === "TRACE");
+      ensure(fgaj_level_to_string(20) === "DEBUG");
+      ensure(fgaj_level_to_string(30) === "INFO");
+      ensure(fgaj_level_to_string(40) === "ERROR");
+      ensure(fgaj_level_to_string(50) === "WARN");
+    }
+  }
+}
+
